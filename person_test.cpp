81679ec2a4f67e1be6e918432a93d9548754b104
Julia Volmer <juliavolmer@t-online.de>
2019-03-24 19:30:52 +0100

    Correct include statement in person test

--- person_test.cpp
+++ person_test.cpp
@@ -1,7 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE persontest
 #include <boost/test/unit_test.hpp>
-#include "store.hpp"
+#include "person.hpp"
 
 BOOST_AUTO_TEST_SUITE( persontest )
 
