b227d0dbe878e7d31ae9e3b33de6a4868ed60cf5
Julia Volmer <juliavolmer@t-online.de>
2019-04-28 21:54:04 +0200

    Rewrite Analyzer tests according to previous changes

--- analyzer_integrationtest.cpp
+++ analyzer_integrationtest.cpp
@@ -14,22 +14,23 @@ BOOST_AUTO_TEST_CASE( test )
 
 BOOST_AUTO_TEST_CASE( computeFoodPlan )
 {
-    Store store("Kueche");
-    store.addGood({"Eiweiss", {1, 0, 0}, 0, 1000});
-    store.addGood({"MUFA", {0, 1, 0}, 0, 1000});
-    store.addGood({"Eicosapentaensaeure", {0, 0, 1}, 0, 1000});
+    Stock *store = new Store{"Kueche"};
+    store->addGood({"Eiweiss", {1, 0, 0}, 0, 1000});
+    store->addGood({"MUFA", {0, 1, 0}, 0, 1000});
+    store->addGood({"Eicosapentaensaeure", {0, 0, 1}, 0, 1000});
  
-    NutritionDemandingUser julia("Julia");
-    julia.setNutritionMinima({1, 1, 1});
+    User *julia = new NutritionDemandingUser{"Julia"};
+    julia->setNutritionMinima({1, 1, 1});
 
-    Analyzer analyzer(store, julia);
+    Solver *solver = new GnuLinearSolver(GLP_MIN);
+    
+    Analyzer analyzer(store, julia, solver);
     analyzer.computeFoodPlan();
 
     std::vector<double> expectedValue{1, 1, 1};
 
     const std::vector<double>& foodAmount = analyzer.getAmounts();
-    for (unsigned i=0;i < foodAmount.size(); i++)
-        BOOST_TEST( foodAmount[i] == expectedValue[i] );
+    BOOST_TEST( foodAmount == expectedValue, boost::test_tools::per_element() );
 }
 
 
--- analyzer_test.cpp
+++ analyzer_test.cpp
@@ -1,6 +1,7 @@
 #define BOOST_TEST_DYN_LINK
 #define BOOST_TEST_MODULE analyzertest
 #include <boost/test/unit_test.hpp>
+#include <memory>
 #include "analyzer.hpp"
 #include "solver.hpp"
 #include "store.hpp"
@@ -8,52 +9,25 @@
 
 BOOST_AUTO_TEST_SUITE( analyzertest )
 
-// MOCK_BASE_CLASS( MockBound, Bound )
-// {
-//     MOCK_METHOD(getType, 0);
-//     MOCK_METHOD(getLower, 0);
-//     MOCK_METHOD(getUpper, 0);
-// };
-
-// MOCK_BASE_CLASS( MockSolver, Solver )
-// {
-//     MOCK_METHOD(getResultVariables, 0);
-//     MOCK_METHOD(getResultValue, 0);
-//     MOCK_METHOD(addProblemCoefficient, 1);
-//     MOCK_METHOD(addConstraintCoefficients, 1);
-//     MOCK_METHOD(addAuxiliaryBound, 1);
-//     MOCK_METHOD(addStructuralBound, 1);
-//     MOCK_METHOD(prepare, 0);
-//     MOCK_METHOD(solve, 0);
-// };
-
-// MOCK_BASE_CLASS( MockStock, Stock )
-// {
-//     MOCK_METHOD(getName, 0);
-//     MOCK_METHOD(getLocation, 0);
-//     MOCK_METHOD(getAllGoods, 0, const std::vector<Food>&());
-//     MOCK_METHOD(addGood, 1);
-// };
-
-// MOCK_BASE_CLASS( MockUser, User )
-// {
-//     MOCK_METHOD(setNutritionMinima, 1);
-//     MOCK_METHOD(getNutritionMinima, 0);
-// };
-
-BOOST_AUTO_TEST_CASE( testConstructor )
+BOOST_AUTO_TEST_CASE( testCallToSolver )
 {
-    MockSolver* solver = new MockSolver;
-    const MockStock* stock = new MockStock;
-    const MockUser* user = new MockUser;
+    auto solver = std::make_shared<MockSolver>();
+    auto stock = std::make_shared<MockStock>();
+    auto user = std::make_shared<MockUser>();
+    
+    stock->addGood({"PureNutrition1", {1, 0, 0}, 0, 100, .5});
+    stock->addGood({"PureNutrition2", {0, 1, 0}, 0, 100, .2});
 
-    Analyzer<MockGood> analyzer(stock, user, solver);
-    // const std::vector<Good> food;
-    // Analyzer analyzer(stock, user, solver);
-    // MOCK_EXPECT( stock->getAllGoods ).once().with( food );
-    // MOCK_EXPECT( solver->solve ).once();
-    // analyzer.computeFoodPlan();
-    // search: how to use mocking with vector (for store.test)
+    Analyzer analyzer(stock, user, solver);
+    analyzer.computeFoodPlan();
+
+    std::vector<double> expectedAmount;
+    for (const Food& food : stock->getAllGoods())
+        expectedAmount.push_back(food.getCost());
+
+    const std::vector<double>& actualAmount = analyzer.getAmounts();
+    BOOST_TEST( expectedAmount == actualAmount, boost::test_tools::per_element() );
+                
 }
 
 BOOST_AUTO_TEST_SUITE_END( )
