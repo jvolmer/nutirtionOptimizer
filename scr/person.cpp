00a69ef85db2986252068b6949372fc0152d9a7c
Julia Volmer <juliavolmer@t-online.de>
2019-11-23 23:10:03 +0100

    Person: add nutrition maximum vector

--- scr/person.cpp
+++ scr/person.cpp
@@ -4,22 +4,27 @@
 #include <string>
 #include <utility>
 
-NutritionDemandingUser::NutritionDemandingUser(std::string name, std::vector<double> nutritionMinimum):
+NutritionDemandingUser::NutritionDemandingUser(std::string name, std::vector<double> nutritionMinimum, std::vector<double> nutritionMaximum):
     m_name {std::move(name)},
-    m_nutritionMinimum {std::move(nutritionMinimum)}
+    m_nutritionMinimum {std::move(nutritionMinimum)},
+    m_nutritionMaximum {std::move(nutritionMaximum)}
 {
 }
 
 Json::Value NutritionDemandingUser::toJson() const
 {
-    Json::Value nutritions;
-    
+    Json::Value nutritionMinima;
     for (unsigned i=0; i<m_nutritionMinimum.size(); i++)
-        nutritions[std::to_string(i)] = m_nutritionMinimum[i];
+        nutritionMinima[std::to_string(i)] = m_nutritionMinimum[i];
+
+    Json::Value nutritionMaxima;
+    for (unsigned i=0; i<m_nutritionMaximum.size(); i++)
+        nutritionMaxima[std::to_string(i)] = m_nutritionMaximum[i];
 
     Json::Value personOut;
     personOut["name"] = m_name;
-    personOut["nutritionMinima"] = nutritions;
+    personOut["nutritionMinima"] = nutritionMinima;
+    personOut["nutritionMaxima"] = nutritionMaxima;
 
     return personOut;
 }
@@ -29,8 +34,14 @@ void NutritionDemandingUser::readFromJson(const Json::Value& personObj)
     m_name = personObj["name"].asString();
     
     m_nutritionMinimum.clear();
-    const Json::Value& n = personObj["nutritionMinima"];
-    for (unsigned i=0; i<n.size(); i++)
-        m_nutritionMinimum.push_back(n[std::to_string(i)].asDouble());
+    m_nutritionMaximum.clear();
     
+    const Json::Value& nMin = personObj["nutritionMinima"];
+    for (unsigned i=0; i<nMin.size(); i++)
+        m_nutritionMinimum.push_back(nMin[std::to_string(i)].asDouble());
+
+    const Json::Value& nMax = personObj["nutritionMaxima"];
+    for (unsigned i=0; i<nMax.size(); i++)
+        m_nutritionMinimum.push_back(nMax[std::to_string(i)].asDouble());
+
 }
--- scr/person.hpp
+++ scr/person.hpp
@@ -9,7 +9,9 @@ class User
 public:
     virtual ~User(){};
     virtual void setNutritionMinima(const std::vector<double>& nutrition) = 0;
+    virtual void setNutritionMaxima(const std::vector<double>& nutrition) = 0;
     virtual const std::vector<double>& getNutritionMinima() const = 0;
+    virtual const std::vector<double>& getNutritionMaxima() const = 0;
     virtual Json::Value toJson() const = 0;
     virtual void readFromJson(const Json::Value& personObj) = 0;
 };
@@ -19,13 +21,16 @@ class NutritionDemandingUser : public User
 private:
     std::string m_name;
     std::vector<double> m_nutritionMinimum;
+    std::vector<double> m_nutritionMaximum;
 
 public:
-    NutritionDemandingUser(std::string name="", std::vector<double> nutritionMinimum={});
+    NutritionDemandingUser(std::string name="", std::vector<double> nutritionMinimum={}, std::vector<double> nutritionMaximum={});
 
     const std::vector<double>& getNutritionMinima() const {return m_nutritionMinimum;}
+    const std::vector<double>& getNutritionMaxima() const {return m_nutritionMaximum;}
 
     void setNutritionMinima(const std::vector<double>& nutrition){m_nutritionMinimum = nutrition;}
+    void setNutritionMaxima(const std::vector<double>& nutrition){m_nutritionMaximum = nutrition;}
 
     Json::Value toJson() const;
     void readFromJson(const Json::Value& personObj);
@@ -35,13 +40,16 @@ class MockUser : public User
 {
 private:
     std::vector<double> m_nutritionMinimum;
+    std::vector<double> m_nutritionMaximum;
     
 public:
     MockUser() {};
     ~MockUser() {};
     
     void setNutritionMinima(const std::vector<double>& nutrition){m_nutritionMinimum = nutrition;}
+    void setNutritionMaxima(const std::vector<double>& nutrition){m_nutritionMaximum = nutrition;}
     const std::vector<double>& getNutritionMinima() const { return m_nutritionMinimum; };
+    const std::vector<double>& getNutritionMaxima() const { return m_nutritionMaximum; };
 
     Json::Value toJson() const { return Json::Value{}; };
     void readFromJson(const Json::Value& personObj) {};
